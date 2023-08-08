66fe71119400dff26c080abfec7dbbed5d1a28f4
Franco Du Plessis <67424625+Franky-Dee@users.noreply.github.com>
2023-08-08 14:52:25 +0200

    Created all 3 training models

---
+++ supportVectorMachines.py
@@ -0,0 +1,69 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_curve, auc
+import matplotlib.pyplot as plt
+
+# Load the dataset (replace 'iris.csv' with your dataset file path)
+feature_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+data = pd.read_csv('iris.data', names=feature_names + ['species'])
+
+# Display the first few rows of the dataset
+print(data.head())
+
+# Check for any missing values
+print(data.isnull().sum())
+
+# Separate features (X) and target (y)
+X = data.drop('species', axis=1)
+y = data['species']
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize the Support Vector Machine classifier
+model = SVC(probability=True, random_state=42)
+
+# Train the model on the training data
+model.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = model.predict(X_test)
+
+# Calculate evaluation metrics
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, average='weighted')
+recall = recall_score(y_test, y_pred, average='weighted')
+f1 = f1_score(y_test, y_pred, average='weighted')
+
+# Calculate ROC curve and AUC score for each class (OvR strategy)
+y_probs = model.predict_proba(X_test)
+n_classes = len(model.classes_)
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+
+for i in range(n_classes):
+    fpr[i], tpr[i], _ = roc_curve(y_test == model.classes_[i], y_probs[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+# Print evaluation metrics
+print(f"Accuracy: {accuracy:.2f}")
+print(f"Precision: {precision:.2f}")
+print(f"Recall: {recall:.2f}")
+print(f"F1 Score: {f1:.2f}")
+
+# Plot ROC curves for each class
+plt.figure()
+colors = ['blue', 'red', 'green']
+for i, color in zip(range(n_classes), colors):
+    plt.plot(fpr[i], tpr[i], color=color, lw=2, label=f'ROC curve (class {model.classes_[i]}) (area = {roc_auc[i]:.2f})')
+
+plt.plot([0, 1], [0, 1], color='gray', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic for each class')
+plt.legend(loc="lower right")
+plt.show()
